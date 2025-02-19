4e6e5e8c9e684de7ba22bd517ec130359816d8da
Carlos RR <carlosrrocha94@gmail.com>
2025-02-19 14:38:20 -0300

    environment

---
+++ environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    DB_HOST:'localhost',
+    DB_PORT:5432,
+    DB_USER:'admin',
+    DB_PASSWORD:'admin',
+    DB_NAME:'task_management',
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,26 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { TaskModule } from './task/task.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { environment } from '../environments/environments';
 
+// Muestra las variables de entorno cargadas
 console.log('Database Config:', {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD ? '********' : 'MISSING', // Oculta la contraseña
-  database: process.env.DB_NAME,
+  host: environment.DB_HOST,
+  port: environment.DB_PORT,
+  user: environment.DB_USER,
+  password: environment.DB_PASSWORD,
+  database: environment.DB_NAME,
 });
-
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD ? String(process.env.DB_PASSWORD) : 'admin',
-      database: process.env.DB_NAME,
+      host: environment.DB_HOST,
+      port: environment.DB_PORT,
+      username: environment.DB_USER,
+      password: environment.DB_PASSWORD,
+      database: environment.DB_NAME,
       autoLoadModels: true,
       synchronize: true,
     }),
