0b4d8ef3e907b155063f2892fbc94e68fe06223b
Carlos RR <carlosrrocha94@gmail.com>
2025-02-19 03:06:20 -0300

    class-validator

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,7 +1,33 @@
+import { IsOptional, IsString, IsBoolean, IsArray, IsDateString, ArrayNotEmpty, IsDate } from 'class-validator';
+
 export class UpdateTaskDto {
-    title?: string;
-    description?: string;
-    completed?: boolean;
-    tags?: string[];
-    dueDate?: Date;
-  }
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  completed?: boolean;
+
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  tags?: string[];
+
+  @IsOptional()
+  @IsDateString()
+  dueDate?: Date;
+
+  // Las fechas `createdAt` y `updatedAt` no deben ser proporcionadas, pero se validan si es necesario
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date;
+
+  @IsOptional()
+  @IsDate()
+  updatedAt?: Date;
+}
